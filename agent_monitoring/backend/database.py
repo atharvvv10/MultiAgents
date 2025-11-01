41a723729697c1bc74e875e7fa1155f49a85989f
Atharv Chougale <160210787+atharvvv10@users.noreply.github.com>
2025-11-01 15:08:57 +0530

    Update database.py

--- agent_monitoring/backend/database.py
+++ agent_monitoring/backend/database.py
@@ -1,8 +1,8 @@
-from sqlalchemy import create_engine, MetaData
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./logs.db"
+DATABASE_URL = "sqlite:///./agent_monitoring.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-metadata = MetaData()
+Base = declarative_base()
